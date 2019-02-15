7ffdcec80aaaba3c5abf56d8f87691702d555c39
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-02-15 03:44:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -59,12 +59,16 @@ def over?(board)
   end
 end
 
-def winner(board)
-if won?(board) == "X"
-  "X"
-elsif won?(board) == "O"
-  "O"
-else
-  nil
-end
+
+def winner (board)
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
 end
